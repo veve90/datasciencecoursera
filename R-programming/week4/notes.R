94d86121debf282df5a3ec44c9f892cfae8e288f
Alina GHERMAN <veve90@users.noreply.github.com>
2016-03-07 21:29:40 +0100

    str

---
+++ R-programming/week4/notes.R
@@ -0,0 +1,18 @@
+# looking at data
+class(my_var)
+dim(my_var)
+nrow(my_var)
+ncol(my_var)
+object.size(my_var)
+names(my_var)
+head(my_var)
+head(my_var,n=10)
+tail(my_var)
+summary(my_var)
+table(my_var$columnName)
+str(my_var)
+
+# simulating
+my_pois <- replicate(100, rpois(5, 10))
+cm <- colMeans(my_pois)
+hist(cm)
